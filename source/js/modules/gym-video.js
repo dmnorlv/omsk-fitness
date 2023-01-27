94fd12c709bda70a89f3b99aad3c7204b6603bdc
dmnorlv <dmnorlv21@gmail.com>
2023-01-27 13:09:01 +0500

    изменил подключение видео

--- source/js/modules/gym-video.js
+++ source/js/modules/gym-video.js
@@ -1,24 +1,43 @@
+const video = document.querySelector('.gym__video-container');
 const background = document.querySelector('.gym__video-background');
 const button = document.querySelector('.gym__video-button');
-const iframe = document.querySelector('.gym__iframe');
 
 const initVideo = () => {
-  if (background && button && iframe) {
-    background.classList.add('gym__video-background-js');
-    button.classList.add('gym__video-button-js');
-    iframe.classList.add('gym__iframe-js');
+  let link = document.querySelector('.gym__link');
+  if (background && button && link && video) {
+    video.classList.add('gym__video-container-js');
+    let id = parseMediaURL(link);
 
-    button.addEventListener('click', onButtonClick);
+    button.addEventListener('click', () => {
+      let iframe = createIframe(id);
+
+      link.remove();
+      button.remove();
+      video.appendChild(iframe);
+    });
+    link.removeAttribute('href');
   }
 };
 
-function onButtonClick() {
-  background.classList.remove('gym__video-background-js');
-  button.classList.remove('gym__video-button-js');
-  iframe.src = 'https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1';
-  iframe.classList.remove('gym__iframe-js');
+function parseMediaURL(link) {
+  return link.getAttribute('href').split('=').pop();
+}
 
-  button.removeEventListener('click', onButtonClick);
+function createIframe(id) {
+  let iframe = document.createElement('iframe');
+
+  iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('allow', 'autoplay');
+  iframe.setAttribute('src', generateURL(id));
+  iframe.classList.add('gym__iframe');
+
+  return iframe;
+}
+
+function generateURL(id) {
+  let query = '?rel=0&showinfo=0&autoplay=1';
+
+  return 'https://www.youtube.com/embed/' + id + query;
 }
 
 export {initVideo};
