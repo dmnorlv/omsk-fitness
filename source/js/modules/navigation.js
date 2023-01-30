89654e8bfb927793df5596e07dfb8be62f967175
dmnorlv <dmnorlv21@gmail.com>
2023-01-30 15:08:47 +0500

    изменил навигацию в шапке

--- source/js/modules/navigation.js
+++ source/js/modules/navigation.js
@@ -1,12 +1,11 @@
-const navigationList = document.querySelector('.page-header__nav-list');
 const imageContainer = document.querySelector('.page-header__content-img-container');
 const navigation = document.querySelector('.page-header__nav');
-
+const linksWrapper = document.querySelector('.page-header__links-wrapper');
 
 const initNav = () => {
   imageContainer.classList.add('page-header__content-img-container-js');
 
-  if (navigationList.children.length > 1) {
+  if (linksWrapper.children.length > 1) {
     imageContainer.classList.remove('page-header__content-img-container-js');
     navigation.classList.add('page-header__nav-js');
   }
